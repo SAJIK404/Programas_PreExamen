3be783e1cd95bae0a133d1aaa1d0f5edd8e8d341
Sajik Strickland <62692615+SAJIK404@users.noreply.github.com>
2025-11-26 12:05:48 -0600

    Create Calculadora.js

---
+++ Calculadora.js
@@ -0,0 +1,38 @@
+// Calculadora basica 
+// codigo para ser ejecutado en Google Apps Script
+
+class Calculadora{
+  constructor(){
+
+  }
+
+  sumar(a,b){
+    const res = a + b
+    Logger.log(a + " + " + b + " = " + res)
+  }
+
+  restar(a,b){
+    const res = a - b
+    Logger.log(a + " - " + b + " = " + res)
+  }
+
+  multiplicar(a,b){
+    const res = a * b
+    Logger.log(a + " * " + b + " = " + res)
+  }
+
+  dividir(a,b){
+    const res = a / b
+    Logger.log(a + " / " + b + " = " + res)
+  }
+
+}
+
+function programa(){
+  const objeto1 = new Calculadora()
+  const suma = objeto1.sumar(6,6)
+  const resta = objeto1.restar(6,6)
+  const multiplicar = objeto1.multiplicar(6,6)
+  const dividir = objeto1.dividir(6,6)
+
+}
